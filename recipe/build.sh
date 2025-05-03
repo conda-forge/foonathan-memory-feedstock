a33207885bd309c9ba913a4aa4957b0d8a8f8ff8
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-05-02 17:31:00 -0700

    Enable build and test to ensure library compatibility in CI environment

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,14 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON \
       -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
       -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF \
-      -DFOONATHAN_MEMORY_BUILD_TESTS=OFF \
+      -DFOONATHAN_MEMORY_BUILD_TESTS=ON \
       -DFOONATHAN_MEMORY_BUILD_TOOLS=ON \
       -DBUILD_SHARED_LIBS=ON
 
 cmake --build . --config Release -- -j$CPU_COUNT
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  ctest --test-dir build --output-on-failure
+fi
+
 cmake --build . --config Release --target install
